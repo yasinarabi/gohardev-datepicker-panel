6fd9c12487042e4133a8ea79bfa1e9f0140381c2
Yasin Arabi <yasinarabi@gmail.com>
2024-05-22 04:21:13 -0700

    Add some basic options

--- src/module.ts
+++ src/module.ts
@@ -4,37 +4,62 @@ import { SimplePanel } from './components/SimplePanel';
 
 export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
   return builder
-    .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
-    })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
-    })
+    // .addTextInput({
+    //   path: 'text',
+    //   name: 'Simple text option',
+    //   description: 'Description of panel option',
+    //   defaultValue: 'Default value of text input option',
+    // })
+    // .addBooleanSwitch({
+    //   path: 'showSeriesCount',
+    //   name: 'Show series counter',
+    //   defaultValue: false,
+    // })
     .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
+      path: 'type',
+      defaultValue: 'DP',
+      name: 'Panel Type',
       settings: {
         options: [
           {
-            value: 'sm',
-            label: 'Small',
+            value: 'DP',
+            label: 'Date Picker',
           },
           {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
+            value: 'C',
+            label: 'Calendar',
           },
         ],
       },
-      showIf: (config) => config.showSeriesCount,
+    })
+    .addRadio({
+      path: 'init',
+      defaultValue: 'now',
+      name: 'Initial Value',
+      settings: {
+        options: [
+          {
+            value: 'now',
+            label: 'Now',
+          },
+          {
+            value: 'null',
+            label: 'Null',
+          },
+          {
+            value: 'custom',
+            label: 'Custom',
+          },
+        ],
+      },
+      // showIf: (config) => config.showSeriesCount,
+    })
+    .addTextInput({
+      path: 'custom_init',
+      name: 'Custom Initial Value',
+      // description: 'Description of panel option',
+      // defaultValue: 'Default value of text input option',
+      showIf: (config) => config.init == 'custom',
+
     });
 });
